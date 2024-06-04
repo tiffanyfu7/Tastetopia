3e8a830a0212c62b770d84be83a77f00cf8793cf
Pine <pinetcht@gmail.com>
2024-06-04 15:02:55 -0400

    rename recipe to recipe detail & add recipe detail component

--- frontend/src/root/Recipes.jsx
+++ frontend/src/root/Recipes.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Navbar } from "../components/Navbar";
+import "../styles/Recipes.css"
 
 export const Recipes = () => {
-<<<<<<< HEAD
   const RecipeDetails = () => {
     return (
       <>
@@ -18,10 +18,10 @@ export const Recipes = () => {
             <div className="RecipeHeader">
               <h2>Pasta</h2>
               <p>
-                <b>Total time:</b>{" "}
+                <b>Total time:</b>
               </p>
               <p>
-                <b>Yield:</b>{" "}
+                <b>Yield:</b>
               </p>
               <p>
                 <b>Full Recipe:</b>
@@ -29,38 +29,15 @@ export const Recipes = () => {
               </p>
             </div>
           </div>
-=======
-  const RecipeDetail = () => {
-    return (
-      <>
-        <button>Back</button>
-        <div className="recipeBox">
-          <img
-            alt="pasta"
-            src="https://food.fnr.sndimg.com/content/dam/images/food/fullset/2021/02/05/Baked-Feta-Pasta-4_s4x3.jpg.rend.hgtvcom.1280.720.suffix/1615916524567.jpeg"
-            width="30%"
-          />
->>>>>>> main
         </div>
       </>
     );
   };
-<<<<<<< HEAD
   return (
     <>
       <Navbar />
       <div>Recipes</div>
       <RecipeDetails />
-=======
-
-  return (
-    <>
-      <Navbar />
-      <div>
-        <h1>Get a Taste</h1>
-      </div>
-      <RecipeDetail />
->>>>>>> main
     </>
   );
 };
