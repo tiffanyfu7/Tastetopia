6936e2b1514a59b72d178bd40cf3f819437f8380
Patrick Yuson <ntu7de@virginia.edu>
2024-06-04 14:27:43 -0400

    got a functioning api call

--- backend/edamamRouter.js
+++ backend/edamamRouter.js
@@ -1,8 +1,30 @@
 import express from 'express';
 import Router from 'express';
+import request from 'request';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const router = Router();
 
+router.get('/search', async (req, res) => {
+    const q = req.body.q;
+    try {
+        
+        request.get(`https://api.edamam.com/search?q=${q}&app_id=${process.env.EDAMAM_APP_ID}&app_key=${process.env.EDAMAM_API_KEY}`, 
+            function(error, response, body) {
+            
+            if (!error && response.statusCode === 200) {
+                res.status(200).json(body);
+            } else {
+                res.status(response.statusCode).send('Error searching Edamam');
+            }
 
+        });
+        
+    } catch (e) {
+        res.status(400).send(e);
+    }
+})
 
 export default router;
