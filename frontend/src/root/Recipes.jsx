0d1bdba4e5da158205c271b903f78fbba18842b1
Pine <pinetcht@gmail.com>
2024-06-04 14:57:03 -0400

    add recipe detail component

--- frontend/src/root/Recipes.jsx
+++ frontend/src/root/Recipes.jsx
@@ -2,10 +2,41 @@ import React from "react";
 import { Navbar } from "../components/Navbar";
 
 export const Recipes = () => {
+  const RecipeDetails = () => {
+    return (
+      <>
+        <button>Back</button>
+        <div>
+          <div>
+            <img
+              alt="pasta"
+              src="https://food.fnr.sndimg.com/content/dam/images/food/fullset/2021/02/05/Baked-Feta-Pasta-4_s4x3.jpg.rend.hgtvcom.1280.720.suffix/1615916524567.jpeg"
+              width="30%"
+            ></img>
+
+            <div className="RecipeHeader">
+              <h2>Pasta</h2>
+              <p>
+                <b>Total time:</b>{" "}
+              </p>
+              <p>
+                <b>Yield:</b>{" "}
+              </p>
+              <p>
+                <b>Full Recipe:</b>
+                <a href="https://www.foodnetwork.com/recipes/food-network-kitchen/baked-feta-pasta-9867689">Food Network</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
   return (
     <>
       <Navbar />
       <div>Recipes</div>
+      <RecipeDetails />
     </>
   );
 };
