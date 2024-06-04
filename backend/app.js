bb4e8d7c27fae9fa12fde036b5f0042fc07414cd
Patrick Yuson <ntu7de@virginia.edu>
2024-06-04 11:01:32 -0400

    Set up express backend with dummy 'hello world' method in app.js

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import OpenAI from 'openai';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import edamamRouter from './edamamRouter.js';
+
+const app = express();
+const port = 8000;
+
+dotenv.config();
+
+// const openai = new OpenAI({
+//     apiKey: process.env.OPENAI_API_KEY,
+// });
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/edamam', edamamRouter);
+
+app.get('/hello-world', async (req, res) => {
+    try {
+        console.log('Hello world');
+        res.status(200).json({ message: 'hello world'});
+    } catch (e) {
+        res.status(400).send(e);
+    }
+}) 
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
---
+++ backend/edamamRouter.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import Router from 'express';
+
+const router = Router();
+
+
+
+export default router;
