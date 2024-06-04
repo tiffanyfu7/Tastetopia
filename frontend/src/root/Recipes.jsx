b0c5054b629a752fbb9da97fabe38e1731ebe721
Pine <pinetcht@gmail.com>
2024-06-04 16:12:23 -0400

    add styling RecipeDetail

--- frontend/src/root/Recipes.jsx
+++ frontend/src/root/Recipes.jsx
@@ -1,21 +1,23 @@
 import React from "react";
 import { Navbar } from "../components/Navbar";
-import "../styles/Recipes.css"
+import "../styles/RecipeDetail.css";
 
 export const Recipes = () => {
+
+
   const RecipeDetails = () => {
     return (
       <>
-        <button>Back</button>
-        <div>
-          <div>
+        <button className="BackButton">Back</button>
+        <div className="RecipeBox">
+          <div className="RecipeHeader">
             <img
               alt="pasta"
               src="https://food.fnr.sndimg.com/content/dam/images/food/fullset/2021/02/05/Baked-Feta-Pasta-4_s4x3.jpg.rend.hgtvcom.1280.720.suffix/1615916524567.jpeg"
-              width="30%"
+              className="RecipeImg"
             ></img>
 
-            <div className="RecipeHeader">
+            <div className="RecipeHeaderText">
               <h2>Pasta</h2>
               <p>
                 <b>Total time:</b>
@@ -25,8 +27,46 @@ export const Recipes = () => {
               </p>
               <p>
                 <b>Full Recipe:</b>
-                <a href="https://www.foodnetwork.com/recipes/food-network-kitchen/baked-feta-pasta-9867689">Food Network</a>
+                <a href="https://www.foodnetwork.com/recipes/food-network-kitchen/baked-feta-pasta-9867689">
+                  Food Network
+                </a>
               </p>
+            </div>
+          </div>
+
+          <div className="RecipeDetailTextBox">
+            <div className="Ingredients">
+                <h4>Ingredients</h4>
+                <ul>
+                    <li>pasta</li>        
+                    <li>pasta</li>  
+                    <li>pasta</li>  
+                </ul>
+            </div>
+            <p><b>Preparation:</b></p>
+            <div className="HorizontalRecipeText">
+              <div className="HealthLabels">
+                <h4>Health Labels: </h4>
+                <ul>
+                  <li>peanut free</li>
+                  <li>peanut free</li>
+                  <li>peanut free</li>
+                </ul>
+              </div>
+              <div className="Nutrition">
+                <h4>Nutrition: </h4>
+                <ul>
+                  <li>
+                    <b>Calories: </b>450
+                  </li>
+                  <li>
+                    <b>Calories: </b>450
+                  </li>
+                  <li>
+                    <b>Calories: </b>450
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
@@ -36,7 +76,7 @@ export const Recipes = () => {
   return (
     <>
       <Navbar />
-      <div>Recipes</div>
+      <div>Recipe</div>
       <RecipeDetails />
     </>
   );
