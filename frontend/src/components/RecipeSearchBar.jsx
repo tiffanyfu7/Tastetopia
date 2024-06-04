adf572ff09036f2ac068017cfacf02f51662d225
Patrick Yuson <ntu7de@virginia.edu>
2024-06-04 15:32:19 -0400

    completed recipe search

--- frontend/src/components/RecipeSearchBar.jsx
+++ frontend/src/components/RecipeSearchBar.jsx
@@ -4,13 +4,15 @@ import axios from 'axios';
 const RecipeSearchBar = () => {
     const [query, setQuery] = useState('');
     const [done, setDone] = useState(false);
+    const [fetchedData, setFetchedData] = useState([]);
 
     const searchEdamam = async () => {
         try {
             const response = await axios.post('http://localhost:8000/edamam/search', {
                 q: query
             });
-            console.log(JSON.parse(response.data));
+            const jsonData = JSON.parse(response.data);
+            setFetchedData(jsonData.hits);
         } catch (error) {
             console.log('Error searching Edamam: ', error);
         }
