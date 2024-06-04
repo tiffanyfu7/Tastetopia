e1d6bb8355a53f0461973255d48558c595caec41
Pine <pinetcht@gmail.com>
2024-06-04 17:50:42 -0400

    made card clickable

--- frontend/src/components/RecipeCard.jsx
+++ frontend/src/components/RecipeCard.jsx
@@ -2,10 +2,11 @@ import { Rating } from '@mui/material'
 import React from 'react'
 import { LIGHTGREEN, GREEN } from '../main'
 
-const RecipeCard = ({ recipe }) => {
+const RecipeCard = ({ recipe, onClick }) => {
   return (
       <div className="recipe-card card"
         style={{ backgroundColor: recipe.uri != null ? GREEN : LIGHTGREEN }}
+        onClick={onClick}
       >
         <img className="recipe-card-image" src={recipe.image} />
           <p className="recipe-card-title">{recipe.title}</p>
--- frontend/src/components/RecipeDetail.jsx
+++ frontend/src/components/RecipeDetail.jsx
@@ -1,73 +1,77 @@
-import React from 'react'
-import { Rating } from '@mui/material'
-import '../styles/RecipeDetails.css';
+import React from "react";
+import { Rating } from "@mui/material";
+import "../styles/RecipeDetail.css";
 
+export const RecipeDetail = ({ recipe, onBackClick }) => {
+  return (
+    <>
+      <button className="BackButton" onClick={ onBackClick }>
+        Back
+      </button>
+      <div className="RecipeBox">
+        <div className="RecipeHeader">
+          <img
+            alt={recipe.title}
+            src={recipe.image}
+            className="RecipeImg"
+          ></img>
 
-const RecipeDetails = () => {
-    return (
-      <>
-        <button className="BackButton">Back</button>
-        <div className="RecipeBox">
-          <div className="RecipeHeader">
-            <img
-              alt="pasta"
-              src="https://food.fnr.sndimg.com/content/dam/images/food/fullset/2021/02/05/Baked-Feta-Pasta-4_s4x3.jpg.rend.hgtvcom.1280.720.suffix/1615916524567.jpeg"
-              className="RecipeImg"
-            ></img>
-
-            <div className="RecipeHeaderText">
-              <h2>Pasta</h2>
-              <p>
-                <b>Total time:</b>
-              </p>
-              <p>
-                <b>Yield:</b>
-              </p>
-              <p>
-                <b>Full Recipe:</b>
-                <a href="https://www.foodnetwork.com/recipes/food-network-kitchen/baked-feta-pasta-9867689">
-                  Food Network
-                </a>
-              </p>
-            </div>
+          <div className="RecipeHeaderText">
+            <h2>{recipe.title}</h2>
+            <h4>{recipe.author}</h4>
+            <p>
+              <b>Total time:</b> {recipe.totalTime}
+            </p>
+            <p>
+              <b>Yield:</b>
+            </p>
+            <p>
+              <b>Full Recipe:</b>
+              <a href="https://www.foodnetwork.com/recipes/food-network-kitchen/baked-feta-pasta-9867689">
+                Food Network
+              </a>
+            </p>
           </div>
+        </div>
 
-          <div className="RecipeDetailTextBox">
-            <div className="Ingredients">
-                <h4>Ingredients</h4>
-                <ul>
-                    <li>pasta</li>        
-                    <li>pasta</li>  
-                    <li>pasta</li>  
-                </ul>
+        <div className="RecipeDetailTextBox">
+          <div className="Ingredients">
+            <h4>Ingredients</h4>
+            <ul>
+              <li>pasta</li>
+              <li>pasta</li>
+              <li>pasta</li>
+            </ul>
+          </div>
+          <p>
+            <b>Preparation:</b>
+          </p>
+          <div className="HorizontalRecipeText">
+            <div className="HealthLabels">
+              <h4>Health Labels: </h4>
+              <ul>
+                {(recipe.healthLabels).map((label, index) => (
+                    <li key={index}>{label}</li>
+                ))}
+              </ul>
             </div>
-            <p><b>Preparation:</b></p>
-            <div className="HorizontalRecipeText">
-              <div className="HealthLabels">
-                <h4>Health Labels: </h4>
-                <ul>
-                  <li>peanut free</li>
-                  <li>peanut free</li>
-                  <li>peanut free</li>
-                </ul>
-              </div>
-              <div className="Nutrition">
-                <h4>Nutrition: </h4>
-                <ul>
-                  <li>
-                    <b>Calories: </b>450
-                  </li>
-                  <li>
-                    <b>Calories: </b>450
-                  </li>
-                  <li>
-                    <b>Calories: </b>450
-                  </li>
-                </ul>
-              </div>
+            <div className="Nutrition">
+              <h4>Nutrition: </h4>
+              <ul>
+                <li>
+                  <b>Calories: </b>450
+                </li>
+                <li>
+                  <b>Calories: </b>450
+                </li>
+                <li>
+                  <b>Calories: </b>450
+                </li>
+              </ul>
             </div>
           </div>
         </div>
-      </>
-    );
-  };
+      </div>
+    </>
+  );
+};
