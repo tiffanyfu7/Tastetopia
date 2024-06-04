cfcbb0af32fd0fe728f2fbda2a459fc4d743c746
Pine <pinetcht@gmail.com>
2024-06-04 14:45:45 -0400

    add styles folder

--- frontend/src/root/Recipes.jsx
+++ frontend/src/root/Recipes.jsx
@@ -2,10 +2,28 @@ import React from "react";
 import { Navbar } from "../components/Navbar";
 
 export const Recipes = () => {
+  const RecipeDetail = () => {
+    return (
+      <>
+        <button>Back</button>
+        <div className="recipeBox">
+          <img
+            alt="pasta"
+            src="https://food.fnr.sndimg.com/content/dam/images/food/fullset/2021/02/05/Baked-Feta-Pasta-4_s4x3.jpg.rend.hgtvcom.1280.720.suffix/1615916524567.jpeg"
+            width="30%"
+          />
+        </div>
+      </>
+    );
+  };
+
   return (
     <>
       <Navbar />
-      <div>Recipes</div>
+      <div>
+        <h1>Get a Taste</h1>
+      </div>
+      <RecipeDetail />
     </>
   );
 };
