250dbb6df591eb9293ea9bc0955b29fed507dded
Omar <hmj9gf@Virginia.edu>
2024-06-05 10:24:24 -0400

    changes

--- frontend/src/root/Homepage.jsx
+++ frontend/src/root/Homepage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Navbar } from "../components/Navbar";
 import HomepageCards from "../components/HomepageCards";
-import { Link } from "react-router-dom";
 
 export const Homepage = () => {
   
