bf4cbe75f42003c29a4d8d9e73a17b34284539a6
Pine <pinetcht@gmail.com>
2024-06-05 11:49:40 -0400

    add chat

--- frontend/src/components/RecipeDetail.jsx
+++ frontend/src/components/RecipeDetail.jsx
@@ -1,75 +1,118 @@
-import React from "react";
+import { React, useState } from "react";
 import { Rating } from "@mui/material";
 import "../styles/RecipeDetail.css";
 
 export const RecipeDetail = ({ recipe, onBackClick }) => {
+  const [chatClicked, setChatClicked] = useState(false);
   return (
     <>
-      <button className="BackButton" onClick={ onBackClick }>
+      <button className="BackButton" onClick={onBackClick}>
         Back
       </button>
-      <div className="RecipeBox">
-        <div className="RecipeHeader">
-          <img
-            alt={recipe.title}
-            src={recipe.image}
-            className="RecipeImg"
-          ></img>
+      <div className="PageContainer">
+        <div className="RecipeBox">
+          <div className="RecipeHeader">
+            <img
+              alt={recipe.title}
+              src={recipe.image}
+              className="RecipeImg"
+            ></img>
 
-          <div className="RecipeHeaderText">
-            <h2>{recipe.title}</h2>
-            <h4>{recipe.author}</h4>
+            <div className="RecipeHeaderText">
+              <h2>{recipe.title}</h2>
+              <h4>{recipe.author}</h4>
+              <p>
+                <b>Total time:</b> {recipe.totalTime}
+              </p>
+              <p>
+                <b>Yield:</b>
+              </p>
+              <p>
+                <b>Full Recipe:</b>
+                <a href="https://www.foodnetwork.com/recipes/food-network-kitchen/baked-feta-pasta-9867689">
+                  Food Network
+                </a>
+              </p>
+            </div>
+          </div>
+
+          <div className="RecipeHeaderDetails">
+            <Rating
+              name="half-rating-read"
+              defaultValue={recipe.rating}
+              precision={0.5}
+              readOnly
+              className="Ratings"
+            />
+            <div className="DietLabels">
+              <b>Diet labels: </b>
+              {recipe.dietLabels}
+            </div>
+          </div>
+
+          <div className="RecipeDetailTextBox">
+            <div className="Ingredients">
+              <h4>Ingredients</h4>
+              <ul>
+                <li>pasta</li>
+                <li>pasta</li>
+                <li>pasta</li>
+              </ul>
+            </div>
             <p>
-              <b>Total time:</b> {recipe.totalTime}
+              <b>Preparation:</b>
             </p>
-            <p>
-              <b>Yield:</b>
-            </p>
-            <p>
-              <b>Full Recipe:</b>
-              <a href="https://www.foodnetwork.com/recipes/food-network-kitchen/baked-feta-pasta-9867689">
-                Food Network
-              </a>
-            </p>
+            <div className="HorizontalRecipeText">
+              <div className="HealthLabels">
+                <h4>Health Labels: </h4>
+                <ul>
+                  {recipe.healthLabels.map((label, index) => (
+                    <li key={index}>{label}</li>
+                  ))}
+                </ul>
+              </div>
+              <div className="Nutrition">
+                <h4>Nutrition: </h4>
+                <ul>
+                  <li>
+                    <b>Calories: </b>450
+                  </li>
+                  <li>
+                    <b>Calories: </b>450
+                  </li>
+                  <li>
+                    <b>Calories: </b>450
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
 
-        <div className="RecipeDetailTextBox">
-          <div className="Ingredients">
-            <h4>Ingredients</h4>
-            <ul>
-              <li>pasta</li>
-              <li>pasta</li>
-              <li>pasta</li>
-            </ul>
+        {console.log(chatClicked)}
+        <div className={chatClicked ? "ExpandedChat" : "CollapsedChat"}>
+          <div className="ChatHeader">
+            <h3>Ask Sous Chef Sue!</h3>
+            <p>
+              Have any questions about the ingredients, recipe, or more? Ask
+              Sue, our OpenAI Chatbot.
+            </p>
           </div>
-          <p>
-            <b>Preparation:</b>
-          </p>
-          <div className="HorizontalRecipeText">
-            <div className="HealthLabels">
-              <h4>Health Labels: </h4>
-              <ul>
-                {(recipe.healthLabels).map((label, index) => (
-                    <li key={index}>{label}</li>
-                ))}
-              </ul>
+          {chatClicked && ( // Use && for brevity
+            <div className="Inbox">
+              <div className="BottomBar"></div>
             </div>
-            <div className="Nutrition">
-              <h4>Nutrition: </h4>
-              <ul>
-                <li>
-                  <b>Calories: </b>450
-                </li>
-                <li>
-                  <b>Calories: </b>450
-                </li>
-                <li>
-                  <b>Calories: </b>450
-                </li>
-              </ul>
+          )}
+          {!chatClicked && ( // Use && for brevity
+            <div className="buttonContainer">
+              <button
+                className="chatButton"
+                onClick={() => setChatClicked(true)}
+              >
+                Start Chatting
+              </button>
             </div>
-          </div>
+          )}
         </div>
       </div>
     </>
