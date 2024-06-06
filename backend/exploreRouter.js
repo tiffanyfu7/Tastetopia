6868a2d5bb124ca88fb4f2a882e9fe0c48d5c684
Patrick Yuson <ntu7de@virginia.edu>
2024-06-06 16:24:18 -0400

    successfully populated the database with default recipes

--- backend/exploreRouter.js
+++ backend/exploreRouter.js
@@ -19,9 +19,59 @@ router.get('/', async (req, res) => {
     }
 });
 
+// Method to extract a recipe's ID
+const extractRecipeId = (uri) => {
+    const parts = uri.split('#');
+    if (parts.length > 1) {
+        const recipePart = parts[1];
+        
+        if (recipePart.startsWith('recipe_')) {
+            return recipePart.substring(7);
+        }
+    }
+    return null;
+};
+
 router.post('/set-default', async (req, res) => {
+    const defaultData = [];
     try {
         // TODO
+        let recipes = [];
+
+        defaultData.hits.forEach(instance => {
+            recipes.push({
+                'title': instance.recipe.label,
+                'author': instance.recipe.source,
+                'image': instance.recipe.image,
+                'sourceURL': instance.recipe.url,
+                'uri': instance.recipe.uri,
+                'totalTime': instance.recipe.totalTime,
+                'yield': instance.recipe.yield,
+                'cuisineType': instance.recipe.cuisineType,
+                'dietLabels': instance.recipe.dietLabels,
+                'healthLabels': instance.recipe.healthLabels,
+                'ingredients': instance.recipe.ingredientLines,
+                'mealType': instance.recipe.mealType,
+                'dishType': instance.recipe.dishType,
+                'calories': instance.recipe.calories,
+                'fat': instance.recipe.digest[0].total,
+                'carbs': instance.recipe.digest[1].total,
+                'protein': instance.recipe.digest[2].total,
+                'rating': 0,
+                'reviews': {},
+                'id': extractRecipeId(instance.recipe.uri),
+                'verified': true,
+                'default': true
+            });
+        });
+        
+        // Add recipes to database
+        recipes.forEach(async (recipe) => {
+            await setDoc(doc(db, 'Recipe', recipe.id), recipe);
+        });
+
+        res.status(200).json({ message: 'Success!' })
+
     } catch (e) {
         res.status(400).send(e);
     }
