36fd84c9fba9d98d85d60a756bbbb63fdeaca32e
Patrick Yuson <ntu7de@virginia.edu>
2024-06-07 09:26:09 -0400

    implemented get user created and saved recipes in backend

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ import openAIRouter from './openAIRouter.js';
 import recipeRouter from './recipeRouter.js';
 import profileRouter from './profileRouter.js';
 import exploreRouter from './exploreRouter.js';
+import cookbookRouter from './cookbookRouter.js';
 
 dotenv.config();
 
@@ -22,6 +23,7 @@ app.use('/openai', openAIRouter);
 app.use('/recipe', recipeRouter);
 app.use('/profile', profileRouter);
 app.use('/explore', exploreRouter);
+app.use('/cookbook', cookbookRouter);
 
 app.get('/hello-world', async (req, res) => {
     try {
