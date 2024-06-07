8bfce0a872e5a6aa28dc12dc5d58957228a1a164
Patrick Yuson <ntu7de@virginia.edu>
2024-06-07 11:15:19 -0400

    updated backend method to fetch saved and created recipes

--- backend/cookbookRouter.js
+++ backend/cookbookRouter.js
@@ -10,10 +10,48 @@ router.get('/get-all/:id', async (req, res) => {
         request.get(`http://localhost:8000/profile/user/${userId}`, function (error, response, body) {
             if (!error && response.statusCode === 200) {
                 const jsonData = JSON.parse(body);
-                res.status(200).json({ 
-                    createdRecipes: jsonData.createdRecipes,
-                    savedRecipes: jsonData.savedRecipes
+                let savedRecipes = [];
+                let createdRecipes = [];
+
+                // Fetch recipe objects for each recipe in savedRecipes
+                const savedRecipePromises = jsonData.savedRecipes.map(recipeId => {
+                    return new Promise ((resolve, reject) => {
+                        request.get(`http://localhost:8000/recipe/${recipeId}`, function (error, response, body) {
+                        if (!error && response.statusCode === 200) {
+                            resolve(JSON.parse(body));
+                        } else {
+                            reject(error);
+                        }
+                    });
+                    })
                 });
+
+                // Fetch recipe objects for each recipe in createdRecipes
+                const createdRecipePromises = jsonData.createdRecipes.map(recipeId => {
+                    return new Promise((resolve, reject) => {
+                        request.get(`http://localhost:8000/recipe/${recipeId}`, function (error, response, body) {
+                            if (!error && response.statusCode === 200) {
+                                resolve(JSON.parse(body));
+                            } else {
+                                reject(error);
+                            }
+                        });
+                    });
+                });
+
+                Promise.all([...savedRecipePromises, ...createdRecipePromises]) 
+                    .then(recipes => {
+                        savedRecipes = recipes.slice(0, jsonData.savedRecipes.length);
+                        createdRecipes = recipes.slice(jsonData.savedRecipes.length);
+                        
+                        res.status(200).json({
+                            savedRecipes: savedRecipes,
+                            createdRecipes: createdRecipes
+                        });
+                    })
+                    .catch(err => {
+                        res.status(400).send(err);
+                    })
             } else {
                 res.status(400).send(error);
             }
